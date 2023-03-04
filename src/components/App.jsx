5d3c6ef4eb0906bba7e3d69b73f340a993e2f9aa
Viktoria <vik25@i.ua>
2023-03-04 13:53:43 +0100

    6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import items from 'data/transactions.json';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/Friends/FriendList';
-import {TransactionHistory} from 'components/TransactionHistory/TransactionHistory'
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 
 
